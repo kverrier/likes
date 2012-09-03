2c83fa156d8f7157fcabe20b736430be6bbe01d2
Kyle Verrier <kyle.verrier@gmail.com>
2012-09-03 14:25:44 -0400

    add random rating game to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
+    #require 'wikipedia'
     @users = User.all
     @likeables = Likeable.all
+    @random_like = Likeable.all(:order => 'RANDOM()', :limit => 1).first
+    @like_image = "image.jpg"
+    #page = Wikipedia.find( 'Getting Things Done' )
+    #@title = page.title
+    #@content = page.content
   end
 end
